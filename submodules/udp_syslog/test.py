b2b96e56f2098125bbd142ea0dd7c643956d7f97
Mike Weiblen <mew@mew.cx>
2022-05-20 03:50:13 -0600

    rm submodules/udp_syslog/

--- submodules/udp_syslog/test.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-
-import syslog_client
-log = syslog_client.Syslog(host="pink", facility=syslog_client.Facility.LOCAL3)
-log.send("udp_syslog : Hi to pink LOCAL3.INFO", syslog_client.Level.INFO)
