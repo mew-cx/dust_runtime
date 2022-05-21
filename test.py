92ab257b71227baf16f649e6437a05319e613373
Mike Weiblen <mew@mew.cx>
2022-05-20 23:50:26 -0600

    test

--- test.py
+++ test.py
@@ -2,4 +2,4 @@
 
 import syslog_client
 log = syslog_client.Syslog(host="pink", facility=syslog_client.Facility.LOCAL3)
-log.send("udp_syslog : Hi to pink LOCAL3.INFO", syslog_client.Severity.INFO)
+log.send("syslog_client Hi pink", syslog_client.Severity.INFO)
