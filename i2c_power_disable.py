2a3a6ca15e1378062ea15b10ba7fa58aba07ccca
Mike Weiblen <mew@mew.cx>
2022-05-22 03:29:48 -0600

    i2c_power_*able.py

---
+++ i2c_power_disable.py
@@ -0,0 +1,6 @@
+import time
+import digitalio
+import board
+
+i2c_power = digitalio.DigitalInOut(board.I2C_POWER)
+i2c_power.switch_to_input()
---
+++ i2c_power_enable.py
@@ -0,0 +1,9 @@
+import time
+import digitalio
+import board
+
+i2c_power = digitalio.DigitalInOut(board.I2C_POWER)
+i2c_power.switch_to_input()
+time.sleep(0.01)  # wait for default value to settle
+rest_level = i2c_power.value
+i2c_power.switch_to_output(value=(not rest_level))
