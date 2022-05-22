b67d973ca189853a56d934eadbd2d01b0fdc722a
Mike Weiblen <mew@mew.cx>
2022-05-21 18:43:34 -0600

    reorg

--- syslog_client.py
+++ syslog_client.py
@@ -1,31 +1,21 @@
 """
-Remote syslog client.
-License: PUBLIC DOMAIN
-Author: Christian Stigen Larsen
-
-For more information, see RFC 3164, 5424.
------------------------------------------------------------------------------
-#! /usr/bin/env python
-import syslog_client
-log = syslog_client.Syslog(host="pink", facility=syslog_client.Facility.LOCAL3)
-log.send("udp_syslog : Hi to pink LOCAL3.INFO", syslog_client.Severity.INFO)
------------------------------------------------------------------------------
+syslog_client.py
+Derived from Author: Christian Stigen Larsen, License: PUBLIC DOMAIN
+See RFC 3164, 5424.
 """
 
 import socket
 
 class Facility:
   "Syslog facilities"
-  KERN, USER, MAIL, DAEMON, AUTH, SYSLOG, \
-  LPR, NEWS, UUCP, CRON, AUTHPRIV, FTP = range(12)
-
-  LOCAL0, LOCAL1, LOCAL2, LOCAL3, \
-  LOCAL4, LOCAL5, LOCAL6, LOCAL7 = range(16, 24)
+  KERN, USER, MAIL, DAEMON, AUTH, SYSLOG, LPR, NEWS, UUCP, CRON, \
+      AUTHPRIV, FTP = range(12)
+  LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, \
+      LOCAL7 = range(16, 24)
 
 class Severity:
   "Syslog severities"
-  EMERG, ALERT, CRIT, ERR, \
-  WARNING, NOTICE, INFO, DEBUG = range(8)
+  EMERG, ALERT, CRIT, ERR, WARNING, NOTICE, INFO, DEBUG = range(8)
 
 class Syslog:
   "A syslog client that logs to a remote server via UDP."
--- test.py
+++ test.py
@@ -2,4 +2,4 @@
 
 import syslog_client
 log = syslog_client.Syslog(host="pink", facility=syslog_client.Facility.LOCAL3)
-log.send("syslog_client Hi pink", syslog_client.Severity.INFO)
+log.send("another test", syslog_client.Severity.WARNING)
