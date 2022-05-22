7a0cc8de1752be606549c9e35673370060162e19
Mike Weiblen <mew@mew.cx>
2022-05-22 01:42:58 -0600

    tweak tcp_client_CircuitPython_NATIVE.py

--- tcp_client_CircuitPython_NATIVE.py
+++ tcp_client_CircuitPython_NATIVE.py
@@ -1,23 +1,23 @@
+# tcp_client_CircuitPython_NATIVE.py
+# from https://github.com/anecdata/Socket
+
 import wifi
 import socketpool
 import ipaddress
 import time
 from secrets import secrets
 
-
-# edit host and port to match server
-HOST = "192.168.10.10"
+HOST = "192.168.1.1"
 PORT = 5000
 TIMEOUT = 5
 INTERVAL = 5
 MAXBUF = 256
 
-
 print("Connecting to wifi")
 wifi.radio.connect(secrets["ssid"], secrets["password"])
-pool = socketpool.SocketPool(wifi.radio)
-
 print("Self IP", wifi.radio.ipv4_address)
+
+pool = socketpool.SocketPool(wifi.radio)
 server_ipv4 = ipaddress.ip_address(pool.getaddrinfo(HOST, PORT)[0][4][0])
 print("Server ping", server_ipv4, wifi.radio.ping(server_ipv4), "ms")
 
