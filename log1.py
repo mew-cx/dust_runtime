469cccb7cc772c37edad97e82c68f91325db3830
Mike Weiblen <mew@mew.cx>
2022-05-31 22:51:32 -0600

    log1.py : refinements

--- log1.py
+++ log1.py
@@ -1,6 +1,4 @@
-# syslog_rfc5424.py
-
-#############################################################################
+# syslog_client_rfc5424.py
 
 class Facility:
     "Syslog facilities"
@@ -20,7 +18,7 @@ def FormatRFC5424( facility = Facility.USER,
                    msgid = None,
                    structured_data = None,
                    msg = None) :
-        return "<{}>1 {} {} {} {} {} {} {}".format(
+        return "<{}>1 {} {} {} {} {} {} {}\n".format(
             (facility << 3) + severity,
             timestamp or "-",
             hostname or "-",
@@ -41,30 +39,43 @@ HOST = "pink"
 PORT = 514
 TIMEOUT = 5  #None
 
-print("connecting to", secrets["ssid"])
+print("connecting to AP", secrets["ssid"])
 wifi.radio.connect(secrets["ssid"], secrets["password"])
-print("Self IP", wifi.radio.ipv4_address)
+print("my ipaddr", wifi.radio.ipv4_address)
 
 pool = socketpool.SocketPool(wifi.radio)
 server_ipv4 = ipaddress.ip_address(pool.getaddrinfo(HOST, PORT)[0][4][0])
-print("Server ping", server_ipv4, ":", wifi.radio.ping(server_ipv4), "ms")
+print("server ipaddr", server_ipv4)
+print("ping time", wifi.radio.ping(server_ipv4), "ms")
 
 with pool.socket(pool.AF_INET, pool.SOCK_STREAM) as s:
+    print("creating socket")
     s = pool.socket(pool.AF_INET, pool.SOCK_STREAM)
     s.settimeout(TIMEOUT)
+    print("connecting to socket")
     s.connect((HOST, PORT))
 
     logmsg = FormatRFC5424(
         facility = Facility.LOCAL3,
         severity = Severity.INFO,
-        timestamp = "2022-05-31T11:22:33Z",
-        app_name = "dust",
+        timestamp = "2022-05-31T22:33:44Z",
         hostname = "1.2.3.4",
+        app_name = "dust",
         procid = "PROCid",
         msgid = "MSGid",
         structured_data = "[x]",
         msg = "This is the real message here")
     print(repr(logmsg))
+    sent = s.send(logmsg)
+    print("sent length : %d\n" % sent)
 
+    logmsg = FormatRFC5424(
+        facility = Facility.LOCAL3,
+        severity = Severity.NOTICE,
+        timestamp = "2022-05-31T22:33:55Z",
+        hostname = wifi.radio.ipv4_address,
+        app_name = "dust2",
+        msg = "Data goes here")
+    print(repr(logmsg))
     sent = s.send(logmsg)
     print("sent length : %d\n" % sent)
