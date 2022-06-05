100f2cb796c12c297755eecf493e3f8d53db3c94
Mike Weiblen <mew@mew.cx>
2022-06-04 23:33:31 -0600

    rtc_test : comments about confusing result

--- rtc_test.py
+++ rtc_test.py
@@ -1,3 +1,6 @@
+# rtc_test.py -- mew@mew.cx 2002-06-04
+# SPDX-FileCopyrightText: 2022 Mike Weiblen
+
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
@@ -6,27 +9,18 @@ import board
 import adafruit_ds1307
 import rtc
 
-t = time.struct_time((
-    2021,  # year
-    2,     # month
-    3,     # date
-    4,     # hour
-    5,     # minute
-    6,     # second
-    2,     # weekday (0=Sun..6=Sat)
-    -1,    # yearday, not used
-    -1     # isdst, not used
-))
-print("t:\t\t", t)
-
 print("time.localtime:\t", time.localtime())
 
 i2c = board.I2C()
 ds = adafruit_ds1307.DS1307(i2c)
-#ds.datetime = t
 print("ds.datetime:\t", ds.datetime)
 
 r1 = rtc.RTC()
 print("r1.datetime1:\t", r1.datetime)
+
+# !!!!! Try to install the ds1307 as the time source.
 rtc.set_time_source(ds)
-print("r1.datetime2:\t", r1.datetime)
+
+# !!!!! shouldn't this now print the DS1307 time??
+r2 = rtc.RTC()
+print("r2.datetime2:\t", r2.datetime)
