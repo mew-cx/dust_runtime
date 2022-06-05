63d088a0bddee3d525d4edb08c9f8d75327403df
Mike Weiblen <mew@mew.cx>
2022-06-05 16:11:16 -0600

    mv misc remainder of dust0404a/ to HIDE/

--- dust0404a/cpu_temperature_f/code.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
-# SPDX-License-Identifier: MIT
-"""CircuitPython CPU temperature example in Fahrenheit"""
-import time
-import microcontroller
-
-while True:
-    print(microcontroller.cpu.temperature * (9 / 5) + 32)
-    time.sleep(0.15)
