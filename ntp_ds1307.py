925979e74cafef840ce44d5dc4c51c0a4033ced1
Mike Weiblen <mew@mew.cx>
2022-06-05 00:06:02 -0600

    ntp_ds1307 : works to set the DS1307

--- ntp_ds1307.py
+++ ntp_ds1307.py
@@ -1,11 +1,14 @@
 # ntp_ds1307.py -- http://mew.cx/ 2022-06-04
+# Set DS1307 RTC from NTP
 
+from secrets import secrets
 import wifi
 import socketpool
+#import ipaddress
 import struct
 import time
-#import ipaddress
-from secrets import secrets
+import board
+import adafruit_ds1307
 
 HOST = "pool.ntp.org"
 PORT = 123
@@ -38,11 +41,15 @@ def GetNtp():
         print("size", size, "address", address)
 
         seconds = struct.unpack_from("!I", packet, offset=len(packet) - 8)[0]
-        #print("seconds", seconds)
         NTP_TO_UNIX_EPOCH = 2208988800  # 1970-01-01 00:00:00
         return time.localtime(seconds - NTP_TO_UNIX_EPOCH)
 
 t = GetNtp()
-print("GetNtp():",repr(t))
+print("GetNtp():\t",repr(t))
+
+i2c = board.I2C()
+ds = adafruit_ds1307.DS1307(i2c)
+ds.datetime = t
+print("ds.datetime:\t", ds.datetime)
 
 # eof
