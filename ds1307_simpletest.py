e70abf57f968ae4945f45576ecdb45c0e076fff3
Mike Weiblen <mew@mew.cx>
2022-06-08 17:34:16 -0600

    ds1307_simpletest.py : fix DoW, other cleanup

--- ds1307_simpletest.py
+++ ds1307_simpletest.py
@@ -1,11 +1,6 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-# Simple demo of reading and writing the time for the DS1307 real-time clock.
-# Change the if False to if True below to set the time, otherwise it will just
-# print the current date and time every second.
-# Notice also comments to adjust for working with hardware vs. software I2C.
-
 import time
 import board
 import adafruit_ds1307
@@ -13,8 +8,10 @@ import adafruit_ds1307
 i2c = board.I2C()
 rtc = adafruit_ds1307.DS1307(i2c)
 
-# Lookup table for names of days (nicer printing).
-days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+def DayOfWeek(wday):
+    # https://docs.python.org/3/library/time.html#time.struct_time
+    # describes tm_wday as "range [0, 6], Monday is 0"
+    return ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")[wday]
 
 if False:
     # you must set year, mon, date, hour, min, sec and weekday
@@ -23,12 +20,10 @@ if False:
     t = time.struct_time((2017, 10, 29, 15, 14, 15, 0, -1, -1))
     print("Setting time to:", t)
     rtc.datetime = t
-    print()
 
 while True:
     t = rtc.datetime
-    # print(t)     # uncomment for debugging
     print("{} {}-{:02}-{:02} {:02}:{:02}:{:02}".format(
-        days[int(t.tm_wday)],
+        DayOfWeek(t.tm_wday),
         t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-    time.sleep(3)
+    time.sleep(1.5)
