7b070d3ba1652b8289e0d8c3be8316fc7dc1e1fe
Mike Weiblen <mew@mew.cx>
2022-06-12 17:38:13 -0600

    log1 tweaks

--- log1.py
+++ log1.py
@@ -1,27 +1,27 @@
-# logger_rfc5424.py
+# rfc5424_formatter.py
 # https://www.rfc-editor.org/rfc/rfc5424.html
 
 import time
 import rtc
 
+__version__ = "0.0.0"
+__repo__ = "https://github.com/mew-cx/CircuitPython_logger_rfc5424"
+
 class Facility:
-    "Syslog facilities, see RFC5424 section 6.2.1"
+    "Syslog facilities, RFC5424 section 6.2.1"
     KERN, USER, MAIL, DAEMON, AUTH, SYSLOG, LPR, NEWS, UUCP, CRON, \
         AUTHPRIV, FTP = range(0,12)
     LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, \
         LOCAL7 = range(16, 24)
 
 class Severity:
-    "Syslog severities, see RFC5424 section 6.2.1"
+    "Syslog severities, RFC5424 section 6.2.1"
     EMERG, ALERT, CRIT, ERR, WARNING, NOTICE, INFO, DEBUG = range(0,8)
 
-def GetRtcTimestamp():
-    "See RFC5424 section 6.2.3"
-    t = time.localtime()   #rtc.RTC.datetime
-    print(repr(t))
-    result = "{}-{:02}-{:02}T{:02}:{:02}:{:02}Z".format(
+def FormatTimestamp(t):
+    "RFC5424 section 6.2.3"
+    result = "{:04}-{:02}-{:02}T{:02}:{:02}:{:02}Z".format(
         t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
-    print(repr(result))
     return result
 
 def FormatRFC5424(facility = Facility.USER,
@@ -33,33 +33,34 @@ def FormatRFC5424(facility = Facility.USER,
                   msgid = None,
                   structured_data = None,
                   msg = None) :
-    "See RFC5424 section 6"
+    "RFC5424 section 6"
 
     # Sect 9.1: RFC5424's VERSION is "1"
+    # Sect 6.2: HEADER MUST be ASCII
     header = "<{}>1 {} {} {} {} {} ".format(
         (facility << 3) + severity,
-        timestamp or GetRtcTimestamp() or "-",
+        timestamp or "-",
         hostname or "-",
         app_name or "-",
         procid or "-",
         msgid or "-")
+    result = header.encode("ascii")
 
     # Sect 6.3: STRUCTURED-DATA has complicated encoding requirements,
-    # so we expect it to already be properly encoded.
+    # so we require it to already be properly encoded.
     if not structured_data:
         structured_data = b"-"
+    result += structured_data
 
-    # Sect 6.2: HEADER MUST be ASCII
-    result = header.encode("ascii") + structured_data
-
-    # Sect 6.4: MSG SHOULD be UTF-8, but MAY be other encoding.
+    # Sect 6.4: # MSG SHOULD be UTF-8, but MAY be other encoding.
     # If using UTF-8, MSG MUST start with Unicode BOM.
+    # Sect 6 ABNF: MSG is optional.
     #enc = "utf-8-sig"
     enc = "ascii"       # we're using ASCII
     if msg:
         result += b" " + msg.encode(enc)
 
-    print(repr(result))
+    #print(repr(result))
     return result
 
 #############################################################################
@@ -92,9 +93,10 @@ with pool.socket(pool.AF_INET, pool.SOCK_STREAM) as s:
     sent = s.send(FormatRFC5424(
         facility = Facility.LOCAL3,
         severity = Severity.INFO,
+        timestamp = FormatTimestamp(time.localtime())
         hostname = wifi.radio.ipv4_address,
         app_name = "dust",
-        procid = "PROCid",
-        msgid = "MSGid",
-        msg = "rtc "+ GetRtcTimestamp()))
+        procid = "procID",
+        msgid = "msgID",
+        msg = "rtc "+ FormatTimestamp(time.localtime())
     print("sent length : %d" % sent)
